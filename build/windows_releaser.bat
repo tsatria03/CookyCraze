c5c6b485504dbbedf46ae5820353097e079add05
Tony Satriano <156674543+tsatria03@users.noreply.github.com>
2026-04-17 22:57:27 -0700

    Build enhancements.

--- build/windows_releaser.bat
+++ build/windows_releaser.bat
@@ -10,10 +10,19 @@ set ARCHIVE=..\releases\archives\%GAME%_windows_portable_password_is_%PASSWORD%.
 set INSTALLER=..\releases\archives\%GAME%_windows_installer_password_is_%PASSWORD%.exe
 
 echo.
+echo Tagging latest commit as %TAG%...
+git tag -f "%TAG%"
+git push origin -f "%TAG%"
+
+echo.
 echo Creating GitHub release %TITLE% with tag %TAG%...
 echo.
 
-"C:\Program Files\GitHub CLI\gh.exe" release create "%TAG%" "%ARCHIVE%" "%INSTALLER%" --title "%TITLE%" --notes ""
+set ASSETS=
+if exist "%ARCHIVE%" set ASSETS=%ASSETS% "%ARCHIVE%"
+if exist "%INSTALLER%" set ASSETS=%ASSETS% "%INSTALLER%"
+
+"C:\Program Files\GitHub CLI\gh.exe" release create "%TAG%"%ASSETS% --title "%TITLE%" --notes ""
 
 echo.
 echo Release complete.
