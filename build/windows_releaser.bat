1333b94f7bc04c05d1faff30449a7c8dc7a79ee0
Tony Satriano <156674543+tsatria03@users.noreply.github.com>
2026-04-17 21:46:11 -0700

    Updated the game's build scripts.

--- build/windows_releaser.bat
+++ build/windows_releaser.bat
@@ -11,5 +11,19 @@ echo.
 "C:\Program Files\7-Zip\7z.exe" a -t7z "%GAME%_windows_portable_password_is_%PASSWORD%.7z" "%WIN_SOURCE%" -mx=9 -m0=LZMA2 -md=64m -mfb=64 -ms=on -mmt=12 -p%PASSWORD% -mhe=on
 
 echo.
-echo Windows portable build complete.
+echo Moving archive to releases\archives...
+if not exist "..\releases\archives" mkdir "..\releases\archives"
+move "%GAME%_windows_portable_password_is_%PASSWORD%.7z" "..\releases\archives\%GAME%_windows_portable_password_is_%PASSWORD%.7z"
+
+echo.
+echo Building Windows installer...
+echo.
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "windows_releaser.iss"
+
+echo.
+echo Moving installer to releases\archives...
+move "%GAME%_windows_installer_password_is_%PASSWORD%.exe" "..\releases\archives\%GAME%_windows_installer_password_is_%PASSWORD%.exe"
+
+echo.
+echo All builds complete.
 pause
