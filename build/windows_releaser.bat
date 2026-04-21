0d06c91b002557be8ce332102d8ef97ea1657251
Tony Satriano <156674543+tsatria03@users.noreply.github.com>
2026-04-21 03:02:01 -0700

    Unified the 2 building arttefacts into one singular file.

--- build/windows_releaser.bat
+++ build/windows_releaser.bat
@@ -3,14 +3,60 @@ cd /d "%~dp0"
 
 set GAME=CookieCraze
 set PASSWORD=CrazeMastery
+set WIN_SOURCE=..\releases\windows\CookieCraze_windows_portable_password_is_CrazeMastery\cycrz
+set ARCHIVE_DIR=..\releases\archives
+set ARCHIVE_NAME=%GAME%_windows_portable_password_is_%PASSWORD%.7z
+set ARCHIVE=%ARCHIVE_DIR%\%ARCHIVE_NAME%
+set INSTALLER=%ARCHIVE_DIR%\%GAME%_windows_installer_password_is_%PASSWORD%.exe
 
 for /f "usebackq delims=" %%v in ("..\docks\version.txt") do set VERSION=%%v
 
 set TITLE=%GAME% V%VERSION%
 set TAG=V%VERSION%0
-set ARCHIVE=..\releases\archives\%GAME%_windows_portable_password_is_%PASSWORD%.7z
-set INSTALLER=..\releases\archives\%GAME%_windows_installer_password_is_%PASSWORD%.exe
 
+echo.
+echo Version: %VERSION%
+echo Tag:     %TAG%
+echo Title:   %TITLE%
+echo.
+
+echo Building Windows portable 7z archive...
+echo.
+
+if exist "%ARCHIVE%" (
+    echo Removing existing archive...
+    del "%ARCHIVE%"
+)
+
+if not exist "%ARCHIVE_DIR%" mkdir "%ARCHIVE_DIR%"
+
+"C:\Program Files\7-Zip\7z.exe" a -t7z "%ARCHIVE_NAME%" "%WIN_SOURCE%" -mx=9 -m0=LZMA2 -md=64m -mfb=64 -ms=on -mmt=12 -p%PASSWORD% -mhe=on
+if errorlevel 1 (
+    echo ERROR: 7z archive build failed.
+    pause
+    exit /b 1
+)
+
+move "%ARCHIVE_NAME%" "%ARCHIVE%"
+if errorlevel 1 (
+    echo ERROR: Failed to move archive to %ARCHIVE_DIR%.
+    pause
+    exit /b 1
+)
+
+echo.
+echo Building Windows installer...
+echo.
+
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q "..\installer\cycrz.iss"
+if errorlevel 1 (
+    echo ERROR: Installer build failed.
+    pause
+    exit /b 1
+)
+
+echo.
+echo All builds complete.
 echo.
 echo Tagging latest commit as %TAG%...
 git tag -f "%TAG%"
